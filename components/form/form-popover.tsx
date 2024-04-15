839943b68612d28752957bde8f75358277051322
김용민 <dydals3440@gmail.com>
2024-04-15 17:02:41 +0900

    feat: Board Server Action component

--- components/form/form-popover.tsx
+++ components/form/form-popover.tsx
@@ -16,6 +16,9 @@ import { FormSubmit } from './form-button';
 import { PopoverClose } from '@radix-ui/react-popover';
 import { Button } from '../ui/button';
 import { X } from 'lucide-react';
+import { FormPicker } from './form-picker';
+import { ElementRef, useRef } from 'react';
+import { useRouter } from 'next/navigation';
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -30,10 +33,14 @@ export const FormPopover = ({
   align,
   sideOffset = 0,
 }: FormPopoverProps) => {
+  const router = useRouter();
+  const closeRef = useRef<ElementRef<'button'>>(null);
+
   const { execute, fieldErrors } = useAction(createBoard, {
     onSuccess: (data) => {
-      console.log({ data });
       toast.success('Board created');
+      closeRef.current?.click();
+      router.push(`/board/${data.id}`);
     },
     onError: (error) => {
       console.log({ error });
@@ -43,7 +50,9 @@ export const FormPopover = ({
 
   const onSubmit = (formData: FormData) => {
     const title = formData.get('title') as string;
-    execute({ title });
+    const image = formData.get('image') as string;
+
+    execute({ title, image });
   };
 
   return (
@@ -58,7 +67,7 @@ export const FormPopover = ({
         <div className='text-sm font-medium text-center text-neutral-600 pb-4'>
           Create board
         </div>
-        <PopoverClose asChild>
+        <PopoverClose ref={closeRef} asChild>
           <Button
             className='h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600'
             variant='ghost'
@@ -68,6 +77,7 @@ export const FormPopover = ({
         </PopoverClose>
         <form action={onSubmit} className='space-y-4'>
           <div className='space-y-4'>
+            <FormPicker id='image' errors={fieldErrors} />
             <FormInput
               id='title'
               label='Board title'
