4226a6b5a7d7c128c6aa9246e4d2adebf1aefd42
김용민 <dydals3440@gmail.com>
2024-04-16 19:18:00 +0900

    feat: List Options Components 복사, 삭제 기능

---
+++ actions/copy-list/index.ts
@@ -0,0 +1,78 @@
+'use server';
+
+import { auth } from '@clerk/nextjs';
+import { InputType, ReturnType } from './types';
+import { db } from '@/lib/db';
+import { revalidatePath } from 'next/cache';
+import { createSafeAction } from '@/lib/create-safe-action';
+import { CopyList } from './schema';
+import { redirect } from 'next/navigation';
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+
+  const { id, boardId } = data;
+  let list;
+
+  try {
+    //  우리가 카피할 리스트 찾음
+    const listToCopy = await db.list.findUnique({
+      where: {
+        id,
+        boardId,
+        board: { orgId },
+      },
+      // list를 카피해도 그 안에 릴레이션 연결해놓은 cards또한 다 가져오기.
+      include: {
+        cards: true,
+      },
+    });
+    if (!listToCopy) {
+      return { error: 'List not found!' };
+    }
+    // 어떤게 라스트 리스트인지 찾음.
+    // boardId 매칭, order프로퍼티 descending 모드, 그리고 order 프로퍼티만 꺼내옴.
+    const lastList = await db.list.findFirst({
+      where: { boardId },
+      orderBy: { order: 'desc' },
+      select: { order: true },
+    });
+
+    const newOrder = lastList ? lastList.order + 1 : 1;
+    list = await db.list.create({
+      data: {
+        boardId: listToCopy.boardId,
+        title: `${listToCopy.title} - Copy`,
+        order: newOrder,
+        cards: {
+          createMany: {
+            data: listToCopy.cards.map((card) => ({
+              title: card.title,
+              description: card.description,
+              order: card.order,
+            })),
+          },
+        },
+      },
+      // 카드를 포함해서 내보냄.
+      include: {
+        cards: true,
+      },
+    });
+  } catch (error) {
+    return {
+      error: 'Failed to copy',
+    };
+  }
+
+  revalidatePath(`/board/${boardId}`);
+  return { data: list };
+};
+
+export const copyList = createSafeAction(CopyList, handler);
