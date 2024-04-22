11c9073f62b4f903ba566981cc914605cc5eb494
김용민 <dydals3440@gmail.com>
2024-04-22 22:46:44 +0900

    feat: 결제 모달 연결

--- components/modals/pro-modal.tsx
+++ components/modals/pro-modal.tsx
@@ -4,9 +4,26 @@ import { useProModal } from '@/hooks/use-pro-modal';
 import { Dialog, DialogContent } from '../ui/dialog';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import { useAction } from '@/hooks/use-action';
+import { stripeRedirect } from '@/actions/stripe-redirect';
+import { toast } from 'sonner';
 
 export const ProModal = () => {
   const proModal = useProModal();
+
+  const { execute, isLoading } = useAction(stripeRedirect, {
+    onSuccess: (data) => {
+      window.location.href = data;
+    },
+    onError: (error) => {
+      toast.error(error);
+    },
+  });
+
+  const onClick = () => {
+    execute({});
+  };
+
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
       <DialogContent className='max-w-md p-0 overflow-hidden'>
@@ -25,7 +42,12 @@ export const ProModal = () => {
             <li>And more!</li>
           </div>
         </div>
-        <Button className='w-full' variant={'primary'}>
+        <Button
+          disabled={isLoading}
+          onClick={onClick}
+          className='w-full'
+          variant={'primary'}
+        >
           Upgrade
         </Button>
       </DialogContent>
